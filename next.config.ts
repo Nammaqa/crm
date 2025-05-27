e98e1353e36d83a0c7fc9c462602087bb04945cf
PriyadarshiniPriya-23 <priya.d@wizzybox.com>
2025-05-27 14:01:35 +0530

    allow cors

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,22 @@
 import type { NextConfig } from "next";
 //access-control-allow-origin
 const nextConfig: NextConfig = {
-  async headers() {
+ async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/api/:path*',
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+      // Add headers for font downloads
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }
         ],
       },
     ];
